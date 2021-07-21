fe269aa39502c5adcc7884002866d86d080096f2
anyfini <38047116+anyfini@users.noreply.github.com>
2021-07-21 22:32:44 +0200

    Readme Updated writingCSVtest.py geupdated und um eine Convertierung in Excel erweitert

--- writingCSVtest.py
+++ writingCSVtest.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import pandas as pd
 
 
 with open('test.csv', 'w', newline='') as csv_file:
@@ -10,3 +11,9 @@ with open('test.csv', 'w', newline='') as csv_file:
         csv_writer.writerow([x, y, 7])
 
     csv_file.close()
+
+read_file = pd.read_csv('test.csv')
+print(read_file)
+writer = pd.ExcelWriter('test.xlsx')
+read_file.to_excel(writer, index=False, header=True)
+writer.save()
